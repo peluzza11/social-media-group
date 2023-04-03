64d0eb7d77d2a394e3dfb87a57d5c53c13edec17
Peluzza11 <ro89585@gmail.com>
2023-04-02 20:14:00 -0700

    yay

--- server/index.js
+++ server/index.js
@@ -1,2 +1,13 @@
 import express from "express";
-  
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer"; /*uploading files*/
+import helmet from "helmet"; /*It helps to protect Node. js Express apps from common security threats*/
+import morgan from "morgan"; /*the process of logging requests to your application.*/
+import path from "path";
+import { fileURLToPath } from "url"; /*properly set the path when configuring directories*/
+
+/*configurations*/
+
